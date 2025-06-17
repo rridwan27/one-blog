730d7add46d12e1238e0a0441cf8ecd2341bc5f1
Mohammad Ridwanur Rahman <ridwanur13@gmail.com>
2025-06-17 23:59:27 +0600

    uploaded banner.jsx as well

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router";
 import Typewriter from "../Typewriter/Typewriter";
+import BannerRight from "../BannerRight/BannerRight";
 
 const Banner = () => {
   return (
@@ -55,11 +56,7 @@ const Banner = () => {
         </div>
 
         <div className="order-1 lg:order-2 flex justify-center">
-          <img
-            src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-            alt="Blog illustration"
-            className="rounded-xl shadow-xl w-full max-w-lg object-cover aspect-video"
-          />
+          <BannerRight />
         </div>
       </div>
     </section>
